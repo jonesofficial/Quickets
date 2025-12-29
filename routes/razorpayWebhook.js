21bc83dd92c8d1da6665361bc08a1252f3623ea3
jonesofficial <jonesofficial30@gmail.com>
2025-12-29 23:11:14 +0530

    razorpay Module Installed

--- routes/razorpayWebhook.js
+++ routes/razorpayWebhook.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const { razorpayWebhook } = require("../lib/flow/Razorpaypayments");
+const { razorpayWebhook } = require("../lib/payments/Razorpay_payments");
 
 /**
  * Razorpay Webhook
